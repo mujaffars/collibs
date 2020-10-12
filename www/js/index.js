236b417cd8019a316272d867fb36eea018d30952
Mujaffar <mujaffar.sanadi01@gmail.com>
2020-10-12 18:57:02 +0530

    asdfafd

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,3 @@
 $(function () {
-    for (var i=1; i<49; i++) {
-        var rowHtml = '';
-        rowHtml += '<div class="picMainDiv picRow">';
-        for (var j=1; j<49; j++) {
-            rowHtml += '<div id="coldv_'+i+'_'+j+'" row="'+i+'" col="'+j+'" class="colDiv"></div>';
-        }
-        rowHtml += '</div>';
-        
-        $('.picMainDiv').append(rowHtml);
-    }    
     
 })
